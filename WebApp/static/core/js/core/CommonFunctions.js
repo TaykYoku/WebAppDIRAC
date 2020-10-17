0a0008b7eb6d6322b27c18d92d924da14299ac96
TaykYoku <yokutayk@gmail.com>
2020-10-18 00:20:12 +0200

    in process oidc

--- WebApp/static/core/js/core/CommonFunctions.js
+++ WebApp/static/core/js/core/CommonFunctions.js
@@ -33,9 +33,11 @@ Ext.define("Ext.dirac.core.CommonFunctions", {
   getAuthorizationServerMetadata: function() {
     var meta = sessionStorage.getItem("AuthServerMetadata");
     if (meta == null) {
+      console.log(GLOBAL.APP.configData.configuration.AuthorizationClient.issuer + '/.well-known/openid-configuration');
       Ext.Ajax.request({
         url: GLOBAL.APP.configData.configuration.AuthorizationClient.issuer + '/.well-known/openid-configuration',
         success: function(response) {
+          console.log(response.responseText);
           meta = Ext.JSON.decode(response.responseText);
         }
       });
