3f391589a918bba050d3dda8d71116e305f9933e
TaykYoku <yokutayk@gmail.com>
2020-10-19 13:51:37 +0200

    in process oidc

--- WebApp/handler/RootHandler.py
+++ WebApp/handler/RootHandler.py
@@ -141,6 +141,9 @@ class RootHandler(WebHandler):
     self.application.addSession(dict(session.update(id=generate_token(30))))
     self.set_secure_cookie('session_id', session.id, secure=True, httponly=True)
     print('NEXT: %s' % session['next'])
+    print('SESSION:')
+    from pprint import pprint
+    pprint(session)
     t = template.Template('''<!DOCTYPE html>
       <html>
         <head>
